6ddd85498ca4f642c3b63087cd9bd724bb565318
vancouver29 <vancouver2990@gmail.com>
2020-08-29 20:14:56 +0200

    11-2 Adding a Link from Welcome to Todo Page and New Todo Attributes

--- todo_app/src/components/todo/TodoApp.js
+++ todo_app/src/components/todo/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class TodoApp extends Component {
   render() {
@@ -28,9 +28,24 @@ class ListTodosComponent extends Component {
     super(props);
     this.state = {
       todos: [
-        { id: 1, description: " Learn to Code" },
-        { id: 2, description: " Become an Expert at React" },
-        { id: 3, description: " Visit Germany" },
+        {
+          id: 1,
+          description: " Learn to Code",
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 2,
+          description: " Become an Expert at React",
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 3,
+          description: " Visit Germany",
+          done: false,
+          targetDate: new Date(),
+        },
       ],
     };
   }
@@ -44,6 +59,8 @@ class ListTodosComponent extends Component {
             <tr>
               <th>id</th>
               <th>description</th>
+              <th>Target Date</th>
+              <th>Is Completed ?</th>
             </tr>
           </thead>
           <tbody>
@@ -51,6 +68,8 @@ class ListTodosComponent extends Component {
               <tr>
                 <td>{todo.id}</td>
                 <td>{todo.description}</td>
+                <td>{todo.targetDate.toString()}</td>
+                <td>{todo.done.toString()}</td>
               </tr>
             ))}
           </tbody>
@@ -62,7 +81,12 @@ class ListTodosComponent extends Component {
 
 class WelcomeComponent extends Component {
   render() {
-    return <div>Welcome {this.props.match.params.name}</div>;
+    return (
+      <div>
+        Welcome {this.props.match.params.name}
+        You can manage your todos <Link to="/todos">here</Link>
+      </div>
+    );
   }
 }
 
