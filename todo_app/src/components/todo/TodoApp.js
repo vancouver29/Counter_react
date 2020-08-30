a31802f31fcca69534014bbcd0fdab11625b9d1c
vancouver29 <vancouver2990@gmail.com>
2020-08-30 13:28:15 +0200

    19- Securing Components using Authenticated Route

--- todo_app/src/components/todo/TodoApp.js
+++ todo_app/src/components/todo/TodoApp.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 // import { withRouter } from "react-router";
 import AuthenticationService from "./AuthenticationService.js";
-
+import AuthenticatedRoute from "./AuthenticatedRoute";
 class TodoApp extends Component {
   render() {
     return (
@@ -13,9 +13,15 @@ class TodoApp extends Component {
             <Switch>
               <Route path="/" exact component={LoginComponent} />
               <Route path="/login" component={LoginComponent} />
-              <Route path="/welcome/:name" component={WelcomeComponent} />
-              <Route path="/todos" component={ListTodosComponent} />
-              <Route path="/logout" component={LogoutComponent} />
+              <AuthenticatedRoute
+                path="/welcome/:name"
+                component={WelcomeComponent}
+              />
+              <AuthenticatedRoute
+                path="/todos"
+                component={ListTodosComponent}
+              />
+              <AuthenticatedRoute path="/logout" component={LogoutComponent} />
               <Route component={ErrorComponent} />
             </Switch>
             <FooterComponent />
