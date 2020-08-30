90cf02eb6d4e2c729dc7b1bbee588a7d255bbbcc
vancouver29 <vancouver2990@gmail.com>
2020-08-30 09:59:13 +0200

    13- Using Bootstrap to Create a Menu with Navigation Links

--- todo_app/src/components/todo/TodoApp.js
+++ todo_app/src/components/todo/TodoApp.js
@@ -28,9 +28,39 @@ class TodoApp extends Component {
 class HeaderComponent extends Component {
   render() {
     return (
-      <div>
-        Header <hr />
-      </div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a href="localhost:3000" className="navbar-brand">
+              vancouver
+            </a>
+          </div>
+          <ul className="navbar-nav">
+            <li>
+              <Link className="nav-link" to="/welcome/vancouver">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/todos">
+                Todos
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav navbar-collapse justify-content-end">
+            <li>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/logout">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
@@ -105,7 +135,7 @@ class WelcomeComponent extends Component {
   render() {
     return (
       <div>
-        Welcome {this.props.match.params.name}
+        Welcome {this.props.match.params.name}! {<br />}
         You can manage your todos <Link to="/todos">here</Link>
       </div>
     );
